e4dae7700016aeb9f83b38991ee53b98284554fa
rtx77 <gulnabd@mail.com>
2022-02-20 00:55:31 +0300

    Server:Added support for multiple clients. Client:The entrance to the room has been added.

--- Client/Main/client.py
+++ Client/Main/client.py
@@ -3,7 +3,8 @@ import socket
 import time
 
 sock = socket.socket()
-st = input("Ip server:")
+# st = input("Ip server:")
+st = 'localhost'
 
 try:
 	sock.connect((st, 9091))
@@ -11,11 +12,18 @@ except:
 	print("Connection error!")
 	exit()
 
+room = input()
+
+def init():#TODO encoding
+    sock.send(room.encode())
+    a = sock.recv(4)
+    if  a == None:
+        exit()
+
+    time.sleep(3)
+
 def function(data_x, data_y):
-    time.sleep(1)
     sock.send(data_x.to_bytes(2, 'big') + data_y.to_bytes(2, 'big'))
-    print(data_x.to_bytes(2, 'big'))
-    print(data_y.to_bytes(2, 'big'))
 
     bata = sock.recv(4)
     bata = [int.from_bytes(bata[:2], byteorder ='big',signed=False),
--- Server/main_server.py
+++ Server/main_server.py
@@ -1,17 +1,58 @@
 import socket
+import time
+from threading import Thread
 
-sock = socket.socket()
-sock.bind(('', 9091))
-sock.listen(5)
+class Network:
+    room_pull = []
 
-#first clients connection
-conn_1, addr_1 = sock.accept()
-conn_2, addr_2 = sock.accept()
+    @classmethod
+    def init(cls):
+        cls.sock = socket.socket()
+        cls.sock.bind(('', 9091))
+        cls.sock.listen(5)
+        cls.init_room()
 
-while True:
-    data_1 = conn_1.recv(4)
-    data_2 = conn_2.recv(4)
-    conn_1.send(data_2)
-    conn_2.send(data_1)
+    @staticmethod
+    def sortt(room_pull, room_namber):
+        for i in room_pull:
+            if i[0] == room_namber:
+                return i
+        return None
 
-    print(f"{type(data_1)} {data_2}" )
+    @classmethod
+    def init_room(cls):
+        while True:
+            conn_1, addr_1 = cls.sock.accept()
+            room_namber = conn_1.recv(2)
+            s = cls.sortt(cls.room_pull, room_namber)
+            if s != None:
+                cls.room_pull.remove(s)
+                # new tread
+                conn_1.send(b'\x66')
+                s[1].send(b'\x66')
+                print("New tread created")
+                th = Thread(target=cls.session_connection, args=(conn_1, s[1]))
+                th.start()
+            else:
+                print("New connection")
+                cls.room_pull.append(tuple((room_namber, conn_1)))
+
+    @classmethod
+    def session_connection(cls, conn_1, conn_2):
+        print("newsession")
+        while True:
+            try:
+                data_1 = conn_1.recv(4)
+                data_2 = conn_2.recv(4)
+                conn_1.send(data_2)
+                conn_2.send(data_1)
+            except:
+                conn_1.close()
+                conn_2.close()
+                print("session connection error!")
+                exit()
+# try:
+#     Network.init()
+# finally:
+#     Network.sock.close()
+Network.init()
